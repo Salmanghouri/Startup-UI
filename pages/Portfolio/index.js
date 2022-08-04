e36433123e58902cbe3044cf51b322679938ef15
Salman Ghouri (Mark2) <109726760+xoftsol@users.noreply.github.com>
2022-08-04 17:28:03 +0500

    new temple

---
+++ pages/Portfolio/index.js
@@ -0,0 +1,361 @@
+import React from 'react'
+
+const index = () => {
+  return (
+   <>
+
+<section className="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] p-10">
+  <div className="container">
+    <div className="flex flex-wrap -mx-4">
+      <div className="w-full px-4">
+        <div className="text-center mx-auto mb-[60px] max-w-[510px]">
+          <span className="font-semibold text-lg text-primary mb-2 block">
+            Our Portfolio
+          </span>
+          <h2 className="
+            font-bold
+            text-3xl
+            sm:text-4xl
+            md:text-[40px]
+            text-dark
+            mb-4
+          ">
+            Our Recent Projects
+          </h2>
+          <p className="text-base text-body-color">
+            There are many variations of passages of Lorem Ipsum available
+            but the majority have suffered alteration in some form.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="flex flex-wrap justify-center -mx-4">
+      <div className="w-full px-4">
+        <ul className="flex flex-wrap justify-center mb-12 space-x-1">
+          <li className="mb-1">
+            <button  className="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            ">
+              All Projects
+            </button>
+          </li>
+          <li className="mb-1">
+            <button className="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            ">
+              Branding
+            </button>
+          </li>
+          <li className="mb-1">
+            <button  className="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            ">
+              Design
+            </button>
+          </li>
+          <li className="mb-1">
+            <button className="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            ">
+              Marketing
+            </button>
+          </li>
+          <li className="mb-1">
+            <button className="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            ">
+              Development
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="flex flex-wrap -mx-4">
+      <div  className="w-full md:w-1/2 xl:w-1/3 px-4">
+        <div className="relative mb-12">
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://cdn.tailgrids.com/1.0/assets/images/portfolio/portfolio-01/image-01.jpg" alt="portfolio" className="w-full" />
+          </div>
+          <div className="
+            text-center
+            bg-white
+            relative
+            z-10
+            py-9
+            px-3
+            rounded-lg
+            shadow-lg
+            mx-7
+            -mt-20
+          ">
+            <span className="text-sm text-primary font-semibold block mb-2">
+              Branding
+            </span>
+            <h3 className="font-bold text-xl text-dark mb-4">
+              Branding Design
+            </h3>
+            <a href="javascript:void(0)" className="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            ">
+              View Details
+            </a>
+          </div>
+        </div>
+      </div>
+      <div  className="w-full md:w-1/2 xl:w-1/3 px-4">
+        <div className="relative mb-12">
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://cdn.tailgrids.com/1.0/assets/images/portfolio/portfolio-01/image-02.jpg" alt="portfolio" className="w-full" />
+          </div>
+          <div className="
+            text-center
+            bg-white
+            relative
+            z-10
+            py-9
+            px-3
+            rounded-lg
+            shadow-lg
+            mx-7
+            -mt-20
+          ">
+            <span className="text-sm text-primary font-semibold block mb-2">
+              Marketing
+            </span>
+            <h3 className="font-bold text-xl text-dark mb-4">
+              Best Marketing tips
+            </h3>
+            <a href="javascript:void(0)" className="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            ">
+              View Details
+            </a>
+          </div>
+        </div>
+      </div>
+      <div  className="w-full md:w-1/2 xl:w-1/3 px-4">
+        <div className="relative mb-12">
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://cdn.tailgrids.com/1.0/assets/images/portfolio/portfolio-01/image-03.jpg" alt="portfolio" className="w-full" />
+          </div>
+          <div className="
+            text-center
+            bg-white
+            relative
+            z-10
+            py-9
+            px-3
+            rounded-lg
+            shadow-lg
+            mx-7
+            -mt-20
+          ">
+            <span className="text-sm text-primary font-semibold block mb-2">
+              Development
+            </span>
+            <h3 className="font-bold text-xl text-dark mb-4">
+              Web Design Trend
+            </h3>
+            <a href="javascript:void(0)" className="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            ">
+              View Details
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 xl:w-1/3 px-4">
+        <div className="relative mb-12">
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://cdn.tailgrids.com/1.0/assets/images/portfolio/portfolio-01/image-04.jpg" alt="portfolio" className="w-full" />
+          </div>
+          <div className="
+            text-center
+            bg-white
+            relative
+            z-10
+            py-9
+            px-3
+            rounded-lg
+            shadow-lg
+            mx-7
+            -mt-20
+          ">
+            <span className="text-sm text-primary font-semibold block mb-2">
+              Design
+            </span>
+            <h3 className="font-bold text-xl text-dark mb-4">
+              Business Card Design
+            </h3>
+            <a href="javascript:void(0)" className="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            ">
+              View Details
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 xl:w-1/3 px-4">
+        <div className="relative mb-12">
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://cdn.tailgrids.com/1.0/assets/images/portfolio/portfolio-01/image-05.jpg" alt="portfolio" className="w-full" />
+          </div>
+          <div className="
+            text-center
+            bg-white
+            relative
+            z-10
+            py-9
+            px-3
+            rounded-lg
+            shadow-lg
+            mx-7
+            -mt-20
+          ">
+            <span className="text-sm text-primary font-semibold block mb-2">
+              Marketing
+            </span>
+            <h3 className="font-bold text-xl text-dark mb-4">
+              Digital marketing
+            </h3>
+            <a href="javascript:void(0)" className="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            ">
+              View Details
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 xl:w-1/3 px-4">
+        <div className="relative mb-12">
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://cdn.tailgrids.com/1.0/assets/images/portfolio/portfolio-01/image-06.jpg" alt="portfolio" className="w-full" />
+          </div>
+          <div className="
+            text-center
+            bg-white
+            relative
+            z-10
+            py-9
+            px-3
+            rounded-lg
+            shadow-lg
+            mx-7
+            -mt-20
+          ">
+            <span className="text-sm text-primary font-semibold block mb-2">
+              Branding
+            </span>
+            <h3 className="font-bold text-xl text-dark mb-4">
+              Creative Agency
+            </h3>
+            <a href="javascript:void(0)" className="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            ">
+              View Details
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+   </>
+  )
+}
+
+export default index
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Navbar from './Components/Navbar/index'
+import Footer from './Components/Footer/index'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Navbar/>
+  <Component {...pageProps} />
+  <Footer/>
+  </>
+  )
 }
 
 export default MyApp
